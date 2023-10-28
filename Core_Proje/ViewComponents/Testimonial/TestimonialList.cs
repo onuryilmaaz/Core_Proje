63a2d1af9def18ed4e9864ea514c6a32f2f22b1b
onuryilmaaz <onuryilm.41@gmail.com>
2023-10-28 14:55:53 +0300

    Temanın ve Default kısmının  düzenlenmesi

---
+++ Core_Proje/ViewComponents/Testimonial/TestimonialList.cs
@@ -0,0 +1,16 @@
+﻿using BusinnessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewComponents.Testimonial
+{
+	public class TestimonialList : ViewComponent
+	{
+		TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
+		public IViewComponentResult Invoke()
+		{
+			var values = testimonialManager.TGetList();
+			return View(values);
+		}
+	}
+}
