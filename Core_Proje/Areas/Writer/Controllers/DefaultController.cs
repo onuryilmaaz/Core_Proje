e85cfe0ca833e3094516f0a97f519909ad9cf4e0
Onur Yılmaz <Onurylmz.41@outlook.com>
2023-12-27 23:14:07 +0300

    Admin Dashboard

--- Core_Proje/Areas/Writer/Controllers/DefaultController.cs
+++ Core_Proje/Areas/Writer/Controllers/DefaultController.cs
@@ -1,15 +1,27 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinnessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Proje.Areas.Writer.Controllers
 {
+	[Area("Writer")]
+	[Authorize]
 	public class DefaultController : Controller
 	{
-		[Area("Writer")]
-		[Authorize]
+		AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementDal());
 		public IActionResult Index()
 		{
-			return View();
+			var values=announcementManager.TGetList();
+			return View(values);
+		}
+
+		[HttpGet]
+		public IActionResult AnnouncementDetails(int id)
+		{
+			Announcement announcement=announcementManager.TGetByID(id);
+			return View(announcement);
 		}
 	}
 }
---
+++ Core_Proje/Areas/Writer/ViewComponents/Notification.cs
@@ -0,0 +1,18 @@
+﻿using BusinnessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Core_Proje.Areas.Writer.ViewComponents
+{
+	public class Notification : ViewComponent
+	{
+		AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementDal());
+		public IViewComponentResult Invoke()
+		{
+			var values=announcementManager.TGetList().Take(5).ToList();
+			return View(values);
+		}
+	}
+}
